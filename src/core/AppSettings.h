bfa602dd4cdd04273d8aba8d70c10c439ce6dc19
E7G <55611737+E7G@users.noreply.github.com>
2026-08-23 09:36:44 +0800

    feat: add persistent application settings

---
+++ src/core/AppSettings.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <QObject>
+#include <QSettings>
+
+namespace quickary {
+
+enum class ThemeMode {
+    System = 0,
+    Dark = 1,
+    Light = 2,
+};
+
+class AppSettings final : public QObject {
+    Q_OBJECT
+public:
+    static AppSettings& instance();
+
+    ThemeMode themeMode() const;
+    bool launchAtStartup() const;
+    bool explorerTypeToSearch() const;
+    bool previewByDefault() const;
+    bool nativePreviewEnabled() const;
+    bool closeAfterActivation() const;
+    int launcherResultLimit() const;
+    int deepSearchResultLimit() const;
+
+    void setThemeMode(ThemeMode value);
+    void setLaunchAtStartup(bool value);
+    void setExplorerTypeToSearch(bool value);
+    void setPreviewByDefault(bool value);
+    void setNativePreviewEnabled(bool value);
+    void setCloseAfterActivation(bool value);
+    void setLauncherResultLimit(int value);
+    void setDeepSearchResultLimit(int value);
+
+    void sync();
+
+signals:
+    void changed();
+
+private:
+    AppSettings();
+    void setValue(const QString& key, const QVariant& value);
+    void updateStartupRegistration();
+
+    QSettings settings_;
+};
+
+} // namespace quickary
